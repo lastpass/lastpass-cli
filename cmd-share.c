7ec77f61e4289eb127033bd141d8bf0f8491bdec
Bob Copeland <copeland@lastpass.com>
2015-11-02 12:27:39 -0500

    clarify the license for most files to GPLv2+ w/ OpenSSL exception
    This clarifies the intent; as pointed out in issue #127, it was
    unclear which files were given the OpenSSL exception.
    
    All files for which LastPass holds 100% copyright are adjusted
    herein.  The rest will have to wait until contributors can affirm
    the change.
    
    Signed-off-by: Bob Copeland <copeland@lastpass.com>

--- cmd-share.c
+++ cmd-share.c
@@ -1,6 +1,26 @@
 /*
- * Copyright 2015 LastPass, Inc.
- * Shared folder support.
+ * commands to manipulate shared folders
+ *
+ * Copyright (C) 2015 LastPass.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * In addition, as a special exception, the copyright holders grant you
+ * additional permission to link or combine this program with the OpenSSL
+ * library and distribute the resulting work.  See the LICENSE.OpenSSL file
+ * in this distribution for more details.
  */
 #include "cmd.h"
 #include "util.h"
