4a56b59d78b42a2d4a8a0ccdf9409cbe247b8478
Bob Copeland <copeland@lastpass.com>
2015-11-26 15:39:57 -0500

    cipher: correct handling of empty strings
    This restores the fix from commit 425e5874448c,
    "blob: don't encrypt empty strings" dealing with folder names.
    
    Signed-off-by: Bob Copeland <copeland@lastpass.com>

--- cipher.c
+++ cipher.c
@@ -363,6 +363,9 @@ char *encrypt_and_base64(const char *str, unsigned const char key[KDF_HASH_LEN])
 	size_t len;
 
 	base64 = xstrdup(str);
+	if (!*base64)
+		return base64;
+
 	len = cipher_aes_encrypt(base64, key, &intermediate);
 	free(base64);
 	base64 = cipher_base64(intermediate, len);
