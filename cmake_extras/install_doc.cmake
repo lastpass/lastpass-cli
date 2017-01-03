a620758e2fb8732593aed2c04dc605d8c9295064
Bob Copeland <copeland@lastpass.com>
2017-01-03 12:39:00 -0500

    build: fix install-doc on OSX
    OSX doesn't support install -D; do it in two steps.
    
    Signed-off-by: Bob Copeland <copeland@lastpass.com>

--- cmake_extras/install_doc.cmake
+++ cmake_extras/install_doc.cmake
@@ -1,1 +1,2 @@
-execute_process(COMMAND install -Dm644 -v ${CMAKE_BINARY_DIR}/lpass.1 $ENV{DESTDIR}${MANDIR}/man1/lpass.1)
+execute_process(COMMAND install -v -d $ENV{DESTDIR}${MANDIR}/man1)
+execute_process(COMMAND install -m 0644 -v ${CMAKE_BINARY_DIR}/lpass.1 $ENV{DESTDIR}${MANDIR}/man1/lpass.1)
