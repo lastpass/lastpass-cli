e59c8cbe99c8ce576bbd330776d75221b207b317
Bob Copeland <copeland@lastpass.com>
2017-01-18 16:45:51 -0500

    test: add some tests of basic functionality
    This adds a bash-driven test infrastructure that uses the lpass-test
    binary to do some simple tests of login, add, edit, show, duplicate,
    and generate.  All commands disable syncing and either operate on the
    local blob records (of added accounts) or the accounts that are
    returned in the mock getaccts function.
    
    Signed-off-by: Bob Copeland <copeland@lastpass.com>

---
+++ test/askpass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. $(dirname $0)/include.sh
+
+echo $TEST_PASS
---
+++ test/assert.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+#
+# various assert functions
+#
+function assert_ne
+{
+	if [[ $1 -eq $2 ]]; then
+		echo "FAIL: \"$1 != $2\" $3"
+		return 1
+	fi
+}
+
+function assert_eq
+{
+	if [[ $1 -ne $2 ]]; then
+		echo "FAIL: \"$1 == $2\" $3"
+		return 1
+	fi
+}
+
+function assert_str_neq
+{
+	if [[ "$1" == "$2" ]]; then
+		echo "FAIL: \"$1 != $2\" $3"
+		return 1
+	fi
+}
+
+function assert_str_eq
+{
+	if [[ "$1" != "$2" ]]; then
+		echo "FAIL: \"$1 == $2\" $3"
+		return 1
+	fi
+}
+
+function assertz
+{
+	assert_eq $1 0 $2
+}
+
+function assert
+{
+	assert_ne $1 0 $2
+}
