lastpass/lastpass-cli

commit 1ae6957ac2fdf2b7352eed028fd66bcd11d31173

support new-style private key format fields
The LastPass plugins now support multiline fields (besides Notes)
if the field is part of a known note template.  This has been used
to capture the embedded newlines in ascii-armored private SSH keys
with the ssh-key note type.

Until now lpass only supported ssh keys stored on a single line,
so add the ability to parse these kinds of strings to enable
support for keys created in the newer plugins.

Signed-off-by: Bob Copeland <copeland@lastpass.com>

blob.c
@@ -1136,7 +1136,7 @@ reencrypt:
 struct account *notes_expand(struct account *acc)
 {
 	struct account *expand;
-	struct field *field;
+	struct field *field = NULL;
 	char *start, *lf, *colon, *name, *value;
 	struct attach *attach, *tmp;
 	char *line = NULL;
@@ -1157,19 +1157,48 @@ struct account *notes_expand(struct account *acc)
 	if (strncmp(acc->note, "NoteType:", 9))
 		return NULL;
 
+	enum note_type note_type = NOTE_TYPE_NONE;
+	lf = strchr(acc->note + 9, '\n');
+	if (lf) {
+		_cleanup_free_ char *type = xstrndup(acc->note + 9, lf - (acc->note + 9));
+		note_type = notes_get_type_by_name(type);
+	}
+
 	for (start = acc->note; ; ) {
-		lf = strchrnul(start, '\n');
-		if (lf - start < 0)
-			lf = NULL;
-		if (lf - start <= 0)
+		name = value = NULL;
+		lf = strchr(start, '\n');
+		if (!lf || lf == start)
 			goto skip;
+
 		line = xstrndup(start, lf - start);
 		colon = strchr(line, ':');
-		if (!colon)
+		if (colon) {
+			name = xstrndup(line, colon - line);
+			value = xstrdup(colon + 1);
+		}
+
+		/*
+		 * Append non-keyed strings to existing field.
+		 * If no field, skip.
+		 */
+		if (!name) {
+			if (field)
+				xstrappendf(&field->value, "\n%s", line);
 			goto skip;
-		*colon = '\0';
-		name = line;
-		value = colon + 1;
+		}
+
+		/*
+		 * If this is a known notetype, append any non-existent
+		 * keys to the existing field.  For example, Proc-Type
+		 * in the ssh private key field goes into private key,
+		 * not a Proc-Type field.
+		 */
+		if (note_type != NOTE_TYPE_NONE &&
+		    !note_has_field(note_type, name) && field) {
+			xstrappendf(&field->value, "\n%s", line);
+			goto skip;
+		}
+
 		if (!strcmp(name, "Username"))
 			expand->username = xstrdup(value);
 		else if (!strcmp(name, "Password"))
@@ -1190,6 +1219,8 @@ struct account *notes_expand(struct account *acc)
 			list_add(&field->list, &expand->field_head);
 		}
 skip:
+		free(value);
+		free(name);
 		free(line);
 		line = NULL;
 		if (!lf || !*lf)

notes.c
@@ -121,6 +121,21 @@ const char *notes_get_name(enum note_type note_type)
 	return note_templates[note_type].name;
 }
 
+bool note_has_field(enum note_type note_type, const char *field)
+{
+	const char **p;
+	if (note_type <= NOTE_TYPE_NONE || note_type >= NUM_NOTE_TYPES)
+		return true;
+
+	p = note_templates[note_type].fields;
+	while (*p) {
+		if (!strcmp(field, *p))
+			return true;
+		p++;
+	}
+	return false;
+}
+
 enum note_type notes_get_type_by_shortname(const char *type_str)
 {
 	BUILD_BUG_ON(ARRAY_SIZE(note_templates) != NUM_NOTE_TYPES);

notes.h
@@ -1,6 +1,8 @@
 #ifndef NOTE_TYPES
 #define NOTE_TYPES
 
+#include <stdbool.h>
+
 enum note_type {
 	NOTE_TYPE_NONE = -1,
 	NOTE_TYPE_AMEX,
@@ -34,6 +36,7 @@ struct note_template {
 extern struct note_template note_templates[];
 
 const char *notes_get_name(enum note_type note_type);
+bool note_has_field(enum note_type note_type, const char *field);
 enum note_type notes_get_type_by_shortname(const char *shortname);
 enum note_type notes_get_type_by_name(const char *type_str);
 char *note_type_usage();

commit 0574dc5a9d8afb9db901bcd2714870d8463092bb

blob: fix parsing for secure notes
The final entry in a secure note doesn't necessarily end in a newline,
but the field parsing code skipped the last line due to a regression
introduced in commit 1ae6957ac2f.  Make sure the final line is
considered.

Fixes #311.

Signed-off-by: Bob Copeland <copeland@lastpass.com>

blob.c
@@ -1166,8 +1166,8 @@ struct account *notes_expand(struct account *acc)
 
 	for (start = acc->note; ; ) {
 		name = value = NULL;
-		lf = strchr(start, '\n');
-		if (!lf || lf == start)
+		lf = strchrnul(start, '\n');
+		if (lf == start)
 			goto skip;
 
 		line = xstrndup(start, lf - start);