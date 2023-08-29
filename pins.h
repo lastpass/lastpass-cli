543e09b083d8fda360c6e0ec37782ecb41d785ac
Stefan Harris <1240279+stfnhh@users.noreply.github.com>
2023-08-29 18:46:37 -0400

    lastpass.eu and lastpass.com thumbprints

--- pins.h
+++ pins.h
@@ -1,8 +1,6 @@
 #ifndef PINS_H
 #define PINS_H
 const char *PK_PINS[] = {
-	/* current lastpass.com primary (Thawte) */
-	"YDjIAXSYj+mh+25FGifAiKN4oNOAj+as6gQv4naQG0M=",
 	/* previous lastpass.com primary (Thawte) */
 	"HXXQgxueCIU5TTLHob/bPbwcKOKw6DkfsTWYHbxbqTY=",
 	/* previous lastpass.eu primary (AddTrust) */
@@ -21,5 +19,9 @@ const char *PK_PINS[] = {
 	"SQAWwwYXoceSd8VNbiyxspGXEjFndkklEO2XzLMts10=",
 	/* future lastpass.eu backup (leaf) */
 	"qr2VCNpUi0PK80PfRyF7lFBIEU1Gzz931k03hrD+xGQ=",
+	/* fix 2023-08-29 lastpass.com (leaf) */
+	"YDjIAXSYj+mh+25FGifAiKN4oNOAj+as6gQv4naQG0M=",
+	/* fix 2023-08-29 lastpass.eu (leaf) */
+	"SjMnNhjAyVM5Yv6O5JaQgNygBTU0wdb8Jz3mfQfTc28="
 };
 #endif
