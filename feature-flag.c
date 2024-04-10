6d896db4f508725822cfab7d7d613927d170814b
Julio Cavalin <julio.cavalin@lastpass.com>
2024-04-10 09:34:57 +0100

    fix doc

--- feature-flag.c
+++ feature-flag.c
@@ -1,7 +1,7 @@
 /*
  * feature flag handling routines
  *
- * Copyright (C) 2014-2024 LastPass.
+ * Copyright (C) 2024 LastPass.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
