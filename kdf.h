e138b3f089c99d0494a4d6a774c677659c764656
Mateus Martins <mateus.martins@lastpass.com>
2024-07-23 15:58:13 +0100

    Deny only logins that make use of the SHA256 flow

--- kdf.h
+++ kdf.h
@@ -6,7 +6,7 @@
 
 #define KDF_HASH_LEN SHA256_DIGEST_LENGTH
 #define KDF_HEX_LEN (KDF_HASH_LEN * 2 + 1)
-#define MINIMUM_ITERATIONS 600000
+#define MINIMUM_ITERATIONS 2
 
 void kdf_login_key(const char *username, const char *password, int iterations, char hex[KDF_HEX_LEN]);
 void kdf_decryption_key(const char *username, const char *password, int iterations, unsigned char hash[KDF_HASH_LEN]);
